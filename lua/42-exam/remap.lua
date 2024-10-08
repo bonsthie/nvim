9d77883f18e9d95fd66b30fe270cd0a6f3b629ec
bonsthie <barnabe.bonnet@gmail.com>
2024-10-08 19:59:39 +0200

    rm oil and add '-' keymap on the basic explorer

--- lua/42-exam/remap.lua
+++ lua/42-exam/remap.lua
@@ -19,4 +19,7 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+vim.keymap.set("n", "-", "<cmd>Ex<CR>")
+
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
