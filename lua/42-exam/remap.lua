65652d4b9f9fd05423901a5f581d3ecb51369aee
bonsthie <barnabe.bonnet@gmail.com>
2024-10-08 19:51:49 +0200

    cleaning usless remap.lua

--- lua/42-exam/remap.lua
+++ lua/42-exam/remap.lua
@@ -1,18 +1,5 @@
 vim.g.mapleader = ' '
 
-local function goto_definition()
-	vim.lsp.buf.definition()
-	vim.cmd('normal! zz')
-end
-
-vim.keymap.set("n", "<leader>gd", goto_definition)
-
-
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("n", "J", "mzJ`z")
-
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
@@ -26,14 +13,9 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format)
-
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
