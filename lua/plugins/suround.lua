1626196f27cf1d823107eb1403db9a01ce684f8a
bonsthie <barnabe.bonnet@gmail.com>
2024-10-08 19:33:08 +0200

    rm useless plug

--- lua/plugins/suround.lua
+++
@@ -1,7 +0,0 @@
-return ({
-	"kylechui/nvim-surround",
-	version = "*",     -- For stability; omit to use `main` branch for latest features
-	config = function()
-		require("nvim-surround").setup({})
-	end
-})
--- lua/plugins/undotree.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"mbbill/undotree",
-	event = "InsertEnter",
-	config = function()
-		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-	end
-}
