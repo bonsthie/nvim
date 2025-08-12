f20c5875e7b544d85178a4e7870178ebc784055d
bonsthie <barnabe.bonnet@gmail.com>
2025-08-12 13:14:53 +0200

    add of new telescope grep option <leader>fd fuzzy find form the current file dir <leader>fp fuzzy find from the parent of the current file dir

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
-    "nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope.nvim",
 	config = function()
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
@@ -8,5 +8,20 @@ return {
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end)
 		vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+
+		vim.keymap.set("n", "<leader>fd", function()
+			require("telescope.builtin").find_files({
+				cwd = vim.fn.expand("%:p:h"), -- directory of current file
+			})
+		end, { desc = "Find files in current file's directory" })
+
+
+		-- Find files from parent dir of current file
+		vim.keymap.set("n", "<leader>fp", function()
+			require("telescope.builtin").find_files({
+				cwd = vim.fn.expand("%:p:h:h"), -- go up one level
+			})
+		end, { desc = "Find files in parent of current file's directory" })
 	end
 }
