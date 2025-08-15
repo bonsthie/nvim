87e97276ba9a15154f72ec98b3b8b7d722c8611e
bonsthie <barnabe.bonnet@gmail.com>
2025-08-15 15:41:05 +0200

    add version check so pepole stop asking me when they use the old versio (i hate you ubuntu)

--- init.lua
+++ init.lua
@@ -6,8 +6,25 @@
 --   By: babonnet <babonnet@42angouleme.fr>         +#+  +:+       +#+        --
 --                                                +#+#+#+#+#+   +#+           --
 --   Created: 2024/04/16 16:29:45 by babonnet          #+#    #+#             --
---   Updated: 2025/01/21 18:55:44 by bonsthie         ###   ########.fr       --
+--   Updated: 2025/08/15 15:39:07 by bonsthie         ###   ########.fr       --
 --                                                                            --
 -- ************************************************************************** --
 
-require('barnabait')
+-- Get version table: {major, minor, patch}
+local version = vim.version()
+
+if version.major == 0 and version.minor < 9 then
+  vim.notify(
+    string.format(
+      "⚠️ Your Neovim version is %d.%d.%d. This config requires at least 0.9.0.",
+      version.major,
+      version.minor,
+      version.patch
+    ),
+    vim.log.levels.WARN
+  )
+  return  -- Stop config loading gracefully
+else
+	require('barnabait')
+end
+
